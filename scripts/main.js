026e697389476cbbc1bd8cca75b7a1173c15d8e3
MyLadySnezhka <mylady.snezhka@gmail.com>
2022-06-08 14:51:24 +0300

    add second variant

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,7 @@ linkfname.addEventListener('change', () => {
 linksname.addEventListener('change', () => {
     suname = linksname.value;
     console.log('Фамилия:', suname);
+    hello(funame, suname);
 })
 
 buttonlink.addEventListener('click', () => {
@@ -26,3 +27,30 @@ function hello(fname, sname) {
 
 hello('юный', 'падаван');
 
+
+//вариант инпутов без кнопки
+
+/*
+
+const linkfname = document.querySelector('input[name="firstname"]');
+const linksname = document.querySelector('input[name="secondname"]');
+
+let funame, suname;
+
+linksname.addEventListener('change', () => {
+    suname = linksname.value;
+    console.log('Фамилия:', suname);
+})
+
+linkfname.addEventListener('change', () => {
+    funame = linkfname.value;
+    console.log('Имя:', funame);
+    hello(funame, suname);
+})
+
+function hello(fname, sname) {
+    console.log('Hello', fname, sname);
+};
+
+
+*/
